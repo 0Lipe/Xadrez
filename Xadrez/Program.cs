092ee6e03c18a89eb993f72988ed9facbd549ba0
Lipe <silvafelipes021@gmail.com>
2024-05-30 15:19:41 -0300

    Add project files.

---
+++ Xadrez/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using tabuleiro;
+using Xadrez;
+using xadrez;
+internal class Program {
+    private static void Main(string[] args) {
+
+
+        try {
+            PartidaDeXadrez partida = new PartidaDeXadrez();
+            while (!partida.terminada) {
+                Console.Clear();
+                Tela.imprimirTabuleiro(partida.tab);
+
+                Console.WriteLine();
+                Console.Write("Origem: ");
+                Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                Console.Write("Destino ");
+                Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                partida.execultaMovimento(origem, destino);
+            }
+        
+        }
+        catch (TabuleiroException e) {
+            Console.WriteLine(e.Message);
+        }
+
+        Console.ReadLine();
+    }
+}
