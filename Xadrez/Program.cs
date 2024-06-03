ff340a04fff44988ebaf913883cc0348abf1c8d9
Lipe <silvafelipes021@gmail.com>
2024-06-03 12:30:17 -0300

    Movimentos possiveis das pecas

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -15,6 +15,12 @@ internal class Program {
                 Console.WriteLine();
                 Console.Write("Origem: ");
                 Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveeis();
+
+                Console.Clear();
+                Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+                
                 Console.Write("Destino ");
                 Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
